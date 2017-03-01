42ee893f0fc54fc3dd654e0697587ca9067f5226
gsimpson <gs@bn.co>
2017-03-01 15:58:43 -0500

    Adding uptime metrics.

--- checks.d/http_check_extended.py
+++ checks.d/http_check_extended.py
@@ -7,6 +7,9 @@
 Dragged from datadog agent http_check here: https://github.com/DataDog/dd-agent/blob/8dfe2251b957039034fd1f199aa8da1158200207/checks.d/http_check.py
 
 Only changes are to always track the metric for response time, while also tagging the status code.
+
+Uptime:
+sum:bn.network.http.response_time.count{status_code:200} by {url}/(sum:bn.network.http.response_time.count{status_code:200} by {url} + sum:bn.network.http.response_time.count{!status_code:200} by {url}) * 100.0
 """
 
 # stdlib
